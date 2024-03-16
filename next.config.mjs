e1705d811c97c8cc9023f244cbf074e1e83577e6
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-03-16 02:31:32 -0300

    update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,24 @@
-  /** @type {import('next').NextConfig} */
-  import {config} from "dotenv"
-  config()
-  const nextConfig = {
-      images: {
-        domains: [
-          "firebasestorage.googleapis.com",
-          "localhost",
-          "as1.ftcdn.net",
-          "www.balancaservice.com.br",
-          "static.ecosweb.com.br",
-          "ph-cdn3.ecosweb.com.br",
-          "example.com",
-        ],
-      },
-      env: {
-          API: process.env.API
-        },
-    };
-    
-    export default nextConfig;
-    
+/** @type {import('next').NextConfig} */
+import { config } from "dotenv";
+config();
+const nextConfig = {
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "localhost",
+      "as1.ftcdn.net",
+      "www.balancaservice.com.br",
+      "static.ecosweb.com.br",
+      "ph-cdn3.ecosweb.com.br",
+      "example.com",
+    ],
+  },
+  env: {
+    API: process.env.API,
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
+
+export default nextConfig;

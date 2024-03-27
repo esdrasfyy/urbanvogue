813b61db00685ef46dd76a56066bac8ec8db16b6
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-03-27 03:32:43 -0300

    setting variables

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,16 @@ const nextConfig = {
       "ph-cdn3.ecosweb.com.br",
       "example.com",
       "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com"
+      "avatars.githubusercontent.com",
     ],
   },
   env: {
     API: process.env.API,
+    HOST: process.env.HOST,
+    SECRET: process.env.SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    OAUTH_REDIRECT_URL: process.env.OAUTH_REDIRECT_URL
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,

c515f2947b2be4b161779355bbf361d33740327f
EsdrasFyy <contatoesdrasoficial@gmail.com>
2024-04-26 19:56:12 -0300

    atualizacao

--- app/(pages)/(forgot-password)/forgot-password/page.tsx
+++ app/(pages)/(forgot-password)/forgot-password/page.tsx
@@ -31,6 +31,7 @@ const ForgotPassword:NextPage = () => {
       componentStep = <SendEmail handleCount={handleCount} />;
       break;
   }
+  
   useEffect(() => {
     console.log(stepCount);
   }, [stepCount]);

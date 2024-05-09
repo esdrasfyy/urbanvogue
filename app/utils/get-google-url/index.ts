3170d019099b5c9f1344781740ab9695cfa2f6b4
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-09 00:20:53 -0300

    Adjustments

--- app/utils/get-google-url/index.ts
+++ app/utils/get-google-url/index.ts
@@ -1,8 +1,11 @@
 function getGoogleOAuthURL() {
   const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
 
+  const redirect_uri =
+    `${process.env.OAUTH_REDIRECT_URL}provider=google` as string;
+
   const options = {
-    redirect_uri: process.env.OAUTH_REDIRECT_URL as string,
+    redirect_uri: redirect_uri,
     client_id: process.env.GOOGLE_CLIENT_ID as string,
     access_type: "offline",
     response_type: "code",
